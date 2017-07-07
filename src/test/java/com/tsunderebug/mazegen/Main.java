0b8d2ec27ff07d89fc1c4dd969e18c75f9ef8509
firestar115 <aprime2003@gmail.com>
2017-07-06 20:06:50 -0700

    Jar setup

--- src/test/java/com/tsunderebug/mazegen/Main.java
+++ src/test/java/com/tsunderebug/mazegen/Main.java
@@ -1,15 +1,7 @@
 package com.tsunderebug.mazegen;
 
 import com.tsunderebug.mazestuff.algorithms.RecursiveBacktracker;
-import com.tsunderebug.mazestuff.cells.PolarCell;
-import com.tsunderebug.mazestuff.mazes.MaskedRectangularSquareCellMaze;
 import com.tsunderebug.mazestuff.mazes.RectangularSquareCellMaze;
-import com.tsunderebug.mazestuff.mazes.hex.RectangularHexagonalCellMaze;
-import com.tsunderebug.mazestuff.mazes.polar.PolarMaze;
-import com.tsunderebug.mazestuff.mazes.tri.MaskedRectangularTriangularCellMaze;
-import com.tsunderebug.mazestuff.mazes.tri.RectangularTriangularCellMaze;
-import com.tsunderebug.mazestuff.mazes.tri.TriangularTriangularCellMaze;
-import com.tsunderebug.mazestuff.utils.Mask;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -19,10 +11,9 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		PolarMaze m = new PolarMaze(10);
+		RectangularSquareCellMaze m = new RectangularSquareCellMaze(21600, 19250);
 		new RecursiveBacktracker().apply(m);
-		ImageIO.write(m.toImage(2, 10, false, Color.cyan), "png", new File("test.png"));
-		ImageIO.write(m.toImage(2, 10, true, Color.green), "png", new File("testc.png"));
+		ImageIO.write(m.toImage(1, 1, 1, false, Color.WHITE), "png", new File("youdidthistoyourself.png"));
 	}
 
 }
